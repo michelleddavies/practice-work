027ea1c1cf45423d701964d3cc5bbc02346cad6a
michelleddavies <michelleddavies4@gmail.com>
2023-03-30 08:54:50 -0400

    c

--- research-spam/main_custom2.py
+++ research-spam/main_custom2.py
@@ -46,7 +46,7 @@ accuracy_adaptive = filter.evaluate_adaptive(state_spaces)
 print("Adaptive AI Accuracy:", accuracy_adaptive)
 
 ## custom system
-
+print("Generating custom system model...")
 # Create a line graph of the accuracy over time for the new model
 # feature_vector - size 1x5
 # feature_matrix - size nx5, n feature_vector elements in feature_matrix
@@ -54,14 +54,32 @@ accuracy_over_time = [] # expecting n data points in time, i.e. array of size n
 detector = SpamCallDetector(n_clusters=3)
 data = detector.generate_data(1000)
 X = detector.data_preprocessing(data)
+# Define the minimum and maximum values for each feature
+a_min, a_max = -1, 0.3
+b_min, b_max = -1, 0.5
+c_min, c_max = 10.0, 50.0
+d_min, d_max = -120.0, -70.0
+e_min, e_max = -3.0, 0.3
+y = 0
+y_tot = np.zeros(len(X)) # Initialize y_tot as an array of zeros with the same length as X
+for i, feature_vector in enumerate(X):
+    print("feat: ", feature_vector)
+    if (feature_vector[0] >= a_min and feature_vector[0] <= a_max) or (feature_vector[1] >= b_min and feature_vector[1] <= b_max) or (feature_vector[2] >= c_min and feature_vector[2] <= c_max) or (feature_vector[3] >= d_min and feature_vector[3] <= d_max) or (feature_vector[4] >= e_min and feature_vector[4] <= e_max):
+        y_tot[i] = 1  # set the i-th element of y_tot to 1 if the feature vector meets the threshold conditions
+print(y_tot, type(y_tot))
+detector.supervised_learning(X, y_tot)
+detector.unsupervised_learning(X)
 for feature_vector in X:
-    print("Generating custom system model...")
-    y = np.random.randint(2, size=len(X)) # randomizing to better suit the use case
-    detector.supervised_learning(X, y)
-    detector.unsupervised_learning(X)
-    detector.adaptive_learning(X, y)
-    preds = detector.integration(X)
-    accuracy = detector.evaluate_accuracy(X, y)
+    if (feature_vector[0] >= a_min and feature_vector[0] <= a_max) and (feature_vector[1] >= b_min and feature_vector[1] <= b_max) and (feature_vector[2] >= c_min and feature_vector[2] <= c_max) and (feature_vector[3] >= d_min and feature_vector[3] <= d_max) and (feature_vector[4] >= e_min and feature_vector[4] <= e_max):
+        y = np.array([1])  # convert y to a 1D numpy array
+    else:
+        y = np.array([0])  # convert y to a 1D numpy array of zeros
+    # Reshape feature vector to a 2D array with one row
+    feature_vector = feature_vector.reshape(1, -1)
+    print(feature_vector, type(feature_vector[0]))
+    detector.adaptive_learning(feature_vector, y)
+    preds = detector.integration(feature_vector)
+    accuracy = detector.evaluate_accuracy(feature_vector, y)
     accuracy_over_time.append(accuracy)
 
 print("My System Accuracy: ", accuracy_over_time)
