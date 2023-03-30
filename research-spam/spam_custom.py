b6bbcbfce9af61a8ee1a3ca864140aab9bfea029
michelleddavies <michelleddavies4@gmail.com>
2023-03-30 13:16:26 -0400

    Update spam_custom.py

--- research-spam/spam_custom.py
+++ research-spam/spam_custom.py
@@ -48,11 +48,7 @@ class SpamCallDetector:
         # Combine predictions
         preds = np.zeros(len(X))
         for i in range(len(X)):
-            if preds_supervised[i] == 1:
-                preds[i] = 1
-            elif preds_unsupervised[i] == self.n_clusters-1:
-                preds[i] = 1
-            elif preds_adaptive[i] == 1:
+            if preds_supervised[i] == 1 and preds_unsupervised[i] == self.n_clusters-1 and preds_adaptive[i] == 1:
                 preds[i] = 1
         return preds
     
